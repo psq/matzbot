32eb17f783d740afa46046855802de44b4fac9b3
ab5tract <ab5tract@localhost.localdomain>
2008-04-15 13:56:34 +0000

    updated test cases for Raw class

--- test/test_raw.rb
+++ test/test_raw.rb
@@ -3,24 +3,31 @@ require File.join(File.dirname(__FILE__), "helper")
 context "A Raw object created from a /msg to a user" do
   
   before(:each) do
-    @msg = ':automatthew!n=matthew@10.0.0.5 PRIVMSG ab5tract :this is a message only to ab5tract'
-    @raw = MatzBot::Raw.new(@msg)
+    @msg1 = ':automatthew!n=matthew@10.0.0.5 PRIVMSG ab5tract :this is a message only to ab5tract'
+    @raw_pm = MatzBot::Raw.new(@msg1)
+    
+    @msg2 = ':automatthew!n=matthew@10.0.0.5 PRIVMSG #waves :this is a message only to #waves'
+    @raw_chan = Matzbot::Raw.new(@msg2)
   end
   
   specify "has a type" do
-    @raw.type.should == :msg
+    @raw_pm.type.should == :pm
+    @raw_chan.type.should == :chan
   end
   
   specify "has a sender" do
-    @raw.sender.should == {:nick=>"automatthew", :name=>"n=matthew", :hostmask=>"10.0.0.5"}
+    @raw_pm.sender.should == {:nick=>"automatthew", :name=>"n=matthew", :hostmask=>"10.0.0.5"}
   end
   
   specify "has a recipent" do
-    @raw.to.should == "ab5tract"
+    @raw_pm.to.should == "ab5tract"
   end
   
+  satisfy "is channel message" do
+    @raw_chan.to.should =~ /\A#/
+  
   specify "has a body" do
-    @raw.body.should == "this is a message only to ab5tract"
+    @raw_pm.body.should == "this is a message only to ab5tract"
   end
   
 end
