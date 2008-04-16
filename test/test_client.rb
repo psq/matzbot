45ecaf7e0f95e7781a4c817ab85185df1fd4d8c4
Matthew King <automatthew@gmail.com>
2008-04-16 15:30:43 -0500

    test grab_info

--- test/test_client.rb
+++ test/test_client.rb
@@ -3,11 +3,13 @@ require File.join(File.dirname(__FILE__), "helper")
 context "The thingamabob we're testing" do
   
   before(:each) do
-    @whatsit = "Foo"
+    @match = ":automatthew!n=matthew@76.210.108.157 PRIVMSG #matzbot :-Okay.  do you have a good example?"
+    @no_match = ":heinlein.freenode.net PONG heinlein.freenode.net :LAG15568992"
   end
   
   specify "does some things we want it to" do
-    @whatsit.should == "Foo"
+    MatzBot::Client.grab_info(@match).should.be.a.kind_of MatchData
+    MatzBot::Client.grab_info(@no_match).should == false
   end
   
 end
